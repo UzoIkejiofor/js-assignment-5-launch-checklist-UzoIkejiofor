62d554c3669319a0f0eefdb708353f6dac4bc230
Uzo <uzoikejiofor@gmail.com>
2022-07-06 21:28:36 -0400

    finally

--- script.js
+++ script.js
@@ -2,6 +2,29 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        let pilot = document.querySelector("input[name=pilotName]");
+        let copilot = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoLevel = document.querySelector("input[name=cargoMass]");
+        let list = document.getElementById("faultyItems");
+        const launchStatus = document.getElementById("launchStatus");
+        if (pilot.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoLevel.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        } else {
+            if (formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) === "Error") {
+                event.preventDefault();
+            } else {
+                launchStatus.innerHTML = "Shuttle is Ready for Launch";
+                launchStatus.style.color = "green";
+                event.preventDefault();
+            };
+        };
+
+    });
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
